6b4944dbd48c26e6e84c86e583db92d6de097b2a
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-07-08 16:05:16 -0300

    feat: get user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', [UserController::class, 'index'])->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register']);
